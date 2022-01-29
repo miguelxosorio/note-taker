90d490983c3f97fc4d038bbbf4b2d72ac947567c
vayne <miguel.osorio19@yahoo.com>
2022-01-28 20:51:13 -0600

    testing post request

--- server.js
+++ server.js
@@ -5,22 +5,61 @@ const path = require('path');
 // init express
 const app = express();
 // importing the db.json which should contain the notes
-const notes = require('./Develop/db/db.json');
+const notes = require('./db/db.json');
 // importing the logger middleware
 const logger = require('./middleware/logger');
+// importing the uuid module - unique ID
+const uuid = require('./helpers/uuid');
+// fs module
+const fs = require('fs');
+
 
 const PORT = process.env.PORT || 3000; // looking for environment variables, called PORT or if not available, 3000
 
 //init middleware
 app.use(logger);
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true })); // It parses incoming requests with urlencoded payloads
+app.use(express.json()); // It parses incoming requests
 
-// get api notes endpoint
+// get request
 app.get('/api/notes', (req, res) => {
    res.json(notes);
 });
 
-// set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+// post request
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    console.log(title, text);
+    console.log(req.body);
+    if(title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+        notes.push(newNote)
+        const response = {
+            status: 'success',
+            body: newNote        
+        };
+        fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+        console.log(response);
+        res.json(response)
+    } else {
+        res.json('error in posting the note');
+    }
+});
+
+// delete request
+app.delete('/api/notes/:id', (req, res) => {
+    
+})
 
 // listener
 app.listen(PORT, () => {
